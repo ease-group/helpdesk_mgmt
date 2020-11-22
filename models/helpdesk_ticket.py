df5ac07b3681e5fc8977aef2721ac5e88bfd7fb4
Ekhlas Mohamed <ekhlas835301@gmail.com>
2020-11-22 12:45:43 +0200

    Slove

--- models/helpdesk_ticket.py
+++ models/helpdesk_ticket.py
@@ -50,8 +50,8 @@ class HelpdeskTicket(models.Model):
     company_id = fields.Many2one(
         comodel_name="res.company",
         string="Company",
-        required=False,
-        # default=lambda self: self.env.company,
+        required=True,
+        default=lambda self: self.env.company,
     )
     channel_id = fields.Many2one(
         comodel_name="helpdesk.ticket.channel",
